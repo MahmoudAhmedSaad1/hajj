5ea3043eda777231b56fd9863f88c464e255805c
MahmoudAhmedSaad1 <mahmoudaboalhassan014@gmail.com>
2025-08-05 00:07:07 +0300

    packedge

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,30 +1,19 @@
-import React from "react";
+
 import Logoforfooter from "./Logoforfooter/Logoforfooter";
 import LinksFooter from "./LinksFooter/LinksFooter";
 import Socialfooter from "./Socialfooter/Socialfooter";
 import CopyRight from "./CopyRight/CopyRight";
 
-
-
-export default function Footer (){
+export default function Footer() {
   return (
     <footer className="bg-[#003366] text-white rounded-t-3xl w-[90%] px-6 mx-auto pt-10 pb-4">
       <div className=" mx-auto flex flex-col items-center gap-6 text-center">
-       <Logoforfooter/>
-
-      
-      <LinksFooter/>
-
-      
-        <Socialfooter/>
-
-        
+        <Logoforfooter />
+        <LinksFooter />
+        <Socialfooter />
         <div className="h-px w-full bg-white/20 mt-4" />
-
-        
-        <CopyRight/>
+        <CopyRight />
       </div>
     </footer>
   );
-};
-
+}
