81b48524ae4a22c85039cc61b73ab26cfc4f88eb
MahmoudAhmedSaad1 <mahmoudaboalhassan014@gmail.com>
2025-08-07 17:55:51 +0300

    packedge

--- src/Components/PassengerDetails/PassengerDetailsheader/PassengerDetailsheader.jsx
+++ src/Components/PassengerDetails/PassengerDetailsheader/PassengerDetailsheader.jsx
@@ -1,33 +1,13 @@
+import Addsondesignheaderstepgold from "../Addson/Addsondesignheaderstepgold/Addsondesignheaderstepgold";
+import Addsondesignheaderstepgray from "../Addson/Addsondesignheaderstepgray/Addsondesignheaderstepgray";
 
-export default function PassengerDetailsheader({ activeTab }) {
+export default function PassengerDetailsheader() {
   return <>
-  <div className="w-full md:w-[70%] px-4">
-      
-      <div className="flex mb-4 space-x-2">
-        <div
-          className={`h-1 w-1/3 rounded ${
-            activeTab === 'booking' ? 'bg-[#E8A861]' : 'bg-gray-200'
-          }`}
-        />
-
-      </div>
-
-
-      <div className="flex justify-between">
+  <div className="flex flex-wrap justify-center items-start md:mb-5">  
+  <Addsondesignheaderstepgold  heading={"Passenger Details"} pharagraph={"Please provide your name and email"} />
   
-        <div className="flex flex-col">
-          <span
-            className="text-sm font-bold pssengerdetails  text-[#003E71] "
-          
-        >
-            Booking Overview
-          </span>
-          <span className="text-xs text-gray-500">
-            Please provide your name and email
-          </span>
-        </div>
-
-      </div>
+      <Addsondesignheaderstepgray heading={"Adds-On"} pharagraph={"Additional items to be ready"} />
+      
     </div>
   </>
 }
